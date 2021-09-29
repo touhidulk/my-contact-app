21e75b4c32a250b34603cd09323eacd25620ec50
touhidul <k.touhidul22@gmail.com>
2021-09-29 10:45:29 +0600

    Coommit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className ="ui fixed menu">
+            <div className ="ui container center">
+                <h2 style={{ color : "darkblue", marginLeft:"42%"}}>Contact Manager</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
